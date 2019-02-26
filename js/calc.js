da9a464338f38f1f245f8fa958eeb543314bcee6
Manuel Antón <manuel.anton@cosmomedia.es>
2019-02-26 13:45:12 +0100

    Más cálculos

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,7 @@
  $(document).ready(function(){
+ 	customRange();
  	calc();
+ 	generarTabla();
  	$(".food-range").change(function(){
  		calc();
  	});
@@ -7,8 +9,18 @@
 
  function calc(){
  	var quantity = 0;
- 	$(".food-range").each(function(){
+ 	$("#food-container .food-range").each(function(){
  		quantity += parseInt($(this).val());
  	});
  	$("#food-quantity").html(quantity);
+ }
+
+ function generarTabla(){
+ 	var prototype = '<div class="food"><img src="images/__img__.svg"><div class="slider-wrapper slider-ghost"><input class="food-range input-range" data-slider-id="ex__id__Slider" type="text" data-slider-min="0" data-slider-tooltip="always" data-slider-max="1000" data-slider-step="1" data-slider-value="0"/></div></div>';
+ 	$.each(food, function(key, value){
+ 		var html = prototype.replace("__img__", value.icono).replace("__id__", key);
+ 		$("#food-container").append(html);
+ 	});
+ 	customRange();
+ 	calc();
  }
---
+++ js/food-data.js
@@ -0,0 +1,34 @@
+/**
+ * Calcio: en mg
+ * Fósforo: en mg
+ * Oxalatos: bajos: 1, medios: 2, altos: 3
+ * HdC, proteinas y grasas: g por cada 100g
+ */
+food = [
+	{
+		"nombre" : "Manzana",
+		"descripcion" : "Una manzana",
+		"icono" : "apple",
+		"color" : "#ff0000",
+		"calcio" : 6,
+		"fosforo" : 11,
+		"oxalatos" : 2,
+		"cal" : 52,
+		"hdc" : 13.8,
+		"grasas" : 0.2,
+		"proteinas" : 0.3
+	},
+	{
+		"nombre" : "Fresa",
+		"descripcion" : "Una fresa",
+		"icono" : "strawberry",
+		"color" : "#ff0000",
+		"calcio" : 16,
+		"fosforo" : 24,
+		"oxalatos" : 2,
+		"cal" : 32,
+		"hdc" : 7.7,
+		"grasas" : 0.3,
+		"proteinas" : 0.7
+	}
+];
